ede65c3e536247fb3ea067bae3f1bf7a6b1063bf
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-10-06 18:35:38 +0530

    Bug Fix

--- app.js
+++ app.js
@@ -9,17 +9,15 @@ const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const Review = require("./models/review");
 
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp")
+  .then(() => {
+    console.log("Database Connection Successfull");
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
-});
 
 const app = express();
 
