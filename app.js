00803dd42a33e5c8c7739d99420b8768641a81fa
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-10-07 08:32:37 +0530

    Express Session Basic

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const campgrounds = require('./routes/campgrounds')
 const reviews = require("./routes/reviews");
+const session = require("express-session");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
@@ -25,7 +26,19 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+  secret: 'thisshouldbeasecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  }
+}
+app.use(session(sessionConfig))
 
 app.use("/campgrounds",campgrounds)
 app.use("/campgrounds/:id/reviews", reviews);
